197fb40577292c87aba909c0e4ff1680c07a5e66
pqxzzz <pericles.code@gmail.com>
2025-09-05 14:29:15 +0100

    change typeormconfig

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,13 +5,9 @@ dotenv.config();
 
 export const typeOrmConfig: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: false, // nunca em produção
+  synchronize: false,
   migrationsRun: true,
   migrations: [__dirname + '/../migrations/*.{js,ts}'],
   logging: true,
