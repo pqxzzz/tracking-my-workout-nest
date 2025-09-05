6e4ebf869249882112a65b8e1e4c5236dbe08274
pqxzzz <pericles.code@gmail.com>
2025-09-05 16:30:27 +0100

    upd typeorm prod || dev

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,14 +3,16 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const typeOrmConfig: DataSourceOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: false,
-  ssl: {
-	  rejectUnauthorized: false,
-  },
+  ssl: isProduction
+    ? { rejectUnauthorized: false } // só ativa SSL no RDS
+    : false, // desativa SSL no local
   migrationsRun: true,
   migrations: [__dirname + '/../migrations/*.{js,ts}'],
   logging: true,
